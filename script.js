092ed1b21425d7c16a89a59f01f7c9ee82cba0c9
Rakibul-98 <rakibul.rupom2001@gmail.com>
2021-10-12 20:35:58 +0600

    deposit,withdraw increment and display value done

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function handleLogin() {
     document.getElementById("login-sec").style.display='none';
     document.getElementById("transaction-sec").style.display='block';
 }
+
+function handleDepositWithdraw(type) {
+    const typedAmount = document.getElementById(type+"-amount");
+    const typedNumber = parseInt(typedAmount.value);
+    const amountValue = document.getElementById(type + "-total");
+    const amountNumber = parseInt(amountValue.innerText);
+    const totalInput = typedNumber + amountNumber;
+    document.getElementById(type + "-total").innerText=totalInput;
+    document.getElementById(type + "-amount").value='';
+}
